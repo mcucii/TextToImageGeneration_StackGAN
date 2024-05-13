71f0bf93e116f8ec034a2bce3301269105171da9
mcucii <masa.cucic@gmail.com>
2024-05-13 16:48:26 +0200

    update dataset file

--- dataset.py
+++ dataset.py
@@ -5,6 +5,7 @@ import pickle
 import numpy as np
 import random
 
+from PIL import Image
 
 import config as cfg
 
@@ -13,17 +14,47 @@ class TextImageDataset(data.Dataset):
     self.img_size = img_size
     self.data = []
     self.data_dir = data_dir
+    self.split = split
     split_dir = os.path.join(data_dir, split)
 
     self.filenames = self.load_filenames(split_dir)
-    self.embeddings = self.load_embedding(split_dir, embedding_filename)
+    self.embedding = self.load_embedding(split_dir, embedding_filename)
     self.class_id = self.load_class_id(split_dir, len(self.filenames))
 
-  def load_filenames(split_dir):
-    pass
+  def load_filenames(self, split_dir):
+    filenames_path = os.path.join(split_dir, "filenames.pickle")
+    with open(filenames_path, "rb") as f:
+      filenames = pickle.load(f)
+    return filenames
+  
+  def load_embedding(self, split_dir, embedding_filename):
+    embedding_path = os.path.join(split_dir, embedding_filename)
+    with open(embedding_path, "rb") as f:
+      embedding = pickle.load(f)
+    return embedding
 
-  def load_embedding(split_dir, embedding_filename):
-    pass
+  def load_class_id(self, split_dir):
+    with open(split_dir + '/class_info.pickle', 'rb') as f:
+      class_id = pickle.load(f, encoding='latin1')   
+    return class_id
+  
 
-  def load_class_id(split_dir):
-    pass
+
+  def get_image(self, path):
+    img = Image.open(path)
+    img = img.resize((64, 64), PIL.Image.BILINEAR)
+    return img
+    
+
+# __getitem__ method in Python -> special method that enables instances of a class to use square bracket notation ([]) for accessing element
+  def __getitem__(self, index):
+    images_path = os.path.join(self.data_dir, "/CUB_200_2011/images")
+    img_name = self.filenames[index]
+    img_path = os.path.join(images_path, img_name)
+    img = self.get_image(img_path)
+
+    embedding = None
+    #embedding = self.embedding[index]
+    # ??
+    return img, embedding
+  
