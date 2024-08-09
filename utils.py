98f5965d65d30b066874250902a330f7de9c91ee
mcucii <masa.cucic@gmail.com>
2024-08-09 19:26:29 +0200

    discriminator and generator loss

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
+import torch
 import torch.nn as nn
+import torchvision.utils as vutils
 
 def weight_initialization(m):
     classname = m.__class__.__name__
@@ -12,31 +14,47 @@ def weight_initialization(m):
         if m.bias is not None:
             nn.init.constant_(m.bias.data, 0.0)
 
+def save_model(netG, netD, epoch, model_dir):
+    torch.save(
+        netG.state_dict(),
+        '%s/netG_epoch_%d.pth' % (model_dir, epoch))
+    torch.save(
+        netD.state_dict(),
+        '%s/netD_epoch_last.pth' % (model_dir))
+    print('Save G/D models')
 
-def discriminatior_loss(netD, real_imgs, fake_imgs, real_labels, fake_labels, mu, gpus):
+
+
+def discriminator_loss(netD, real_imgs, fake_imgs, real_labels, fake_labels, conditions):
     criterion = nn.BCELoss()
-    device = real_imgs.device
-    real_labels = real_labels.to(device)
-    fake_labels = fake_labels.to(device)
+    cond = conditions.detach()
+    fake = fake_imgs.detach()
 
-    real_outputs = nn.parallel.data_parallel(netD, real_imgs, device_ids=gpus)
-    errD_real = criterion(real_outputs, real_labels)
+    real_logits = netD(real_imgs, cond)
+    real_labels = real_labels.unsqueeze(1) 
+    errD_real = criterion(real_logits, real_labels)
 
-    fake_outputs = nn.parallel.data_parallel(netD, fake_imgs, device_ids=gpus)
-    errD_fake = criterion(fake_outputs, fake_labels)
-               
-    return errD_real, errD_fake, errD_real.data, errD_fake.data
+    fake_logits = netD(fake, cond)
+    fake_labels = fake_labels.unsqueeze(1) 
+    errD_fake = criterion(fake_logits, fake_labels)
+
+    errD = errD_real + errD_fake
+    return errD
 
 
-
-def generator_loss(netD, fake_imgs, real_labels, conditions, gpus):
+def generator_loss(netD, fake_imgs, real_labels, conditions):
     criterion = nn.BCELoss()
-    device = fake_imgs.device
-    real_labels = real_labels.to(device)
-
-    outputs = nn.parallel.data_parallel(netD, fake_imgs, device_ids=gpus)
-
-    # The generator loss - racuna se na osnovu toga koliko dobro diskriminator moze da oceni da su fejk slike stvarno fejk! 
-    errD_fake = criterion(outputs, real_labels)
-
+    cond = conditions.detach()
+    
+    fake_logits = netD(fake_imgs, cond)
+    real_labels = real_labels.unsqueeze(1)
+    errD_fake = criterion(fake_logits, real_labels)
+    
     return errD_fake
+
+
+def KL_loss(mu, logvar):
+    # -0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    KLD_element = mu.pow(2).add_(logvar.exp()).mul_(-1).add_(1).add_(logvar)
+    KLD = torch.mean(KLD_element).mul_(-0.5)
+    return KLD
