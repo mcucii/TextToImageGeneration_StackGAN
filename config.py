5a869c30835e91f56e50a3aa19af95a38eb47ccf
mcucii <masa.cucic@gmail.com>
2024-08-16 23:49:48 +0200

    config changes

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 DATASET_NAME = 'birds'
 EMBEDDING_TYPE = 'cnn-rnn'
 
-NET_G = ''
+NET_G = ''  # putanja do fajla sa težinama modela
 NET_D = ''
 STAGE1_G = ''
+STAGE1_D = ''
 DATA_DIR =  '/content/drive/MyDrive/RIProject/data_reduced/birds'
 VAL_DIR = ''
 VIS_COUNT = 64
@@ -15,10 +16,9 @@ STAGE = 1
 TRAIN = True
 TRAIN_BATCH_SIZE = 32
 TRAIN_MAX_EPOCH = 100
-TRAIN_SNAPSHOT_INTERVAL = 5
 TRAIN_PRETRAINED_MODEL = ''
 TRAIN_PRETRAINED_EPOCH = 600
-TRAIN_LR_DECAY_EPOCH = 20
+TRAIN_LR_DECAY_EPOCH = 30    # posle koliko epoha se koeficijent ucenja smanjuje
 TRAIN_DISCRIMINATOR_LR = 0.0002
 TRAIN_GENERATOR_LR = 0.0002
 
